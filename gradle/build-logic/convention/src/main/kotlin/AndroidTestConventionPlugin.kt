e66ab4bab673979a37dd79ae6e12464c243775a2
Saket Narayan <saket@saket.me>
2024-12-06 13:39:09 -0500

    Revert to a fixed number of shards on emulator.wtf

--- gradle/build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
@@ -67,7 +67,7 @@ class AndroidTestConventionPlugin : Plugin<Project> {
       )
       @Suppress("SdCardPath")
       directoriesToPull.set(listOf("/sdcard/Download/"))
-      shardTargetRuntime.set(2) // 2 minutes.
+      numUniformShards.set(3)
       numFlakyTestAttempts.set(2) // 3 runs in total.
       fileCacheTtl.set(Duration.ofDays(30))
       timeout.set(Duration.ofMinutes(15)) // Note to self: this is per shard and not per test.
